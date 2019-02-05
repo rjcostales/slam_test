d2fabcf857e7af4310497c4863ebce01e7243af2
Jesse Costales <rjcostales@gmail.com>
2019-02-05 02:56:24 +0000

    update io

--- io/test.sh
+++ io/test.sh
@@ -1,12 +1,15 @@
 clear
 echo 'write test'
 time ./write > test.txt
+
 echo ''
 echo 'read test'
 time ./read < test.txt
+
 echo ''
 echo 'copy test'
 time cp test.txt copy.txt
+
 echo ''
 echo 'remove test'
 time rm test.txt copy.txt
--- io/write.sh
+++ io/write.sh
@@ -1,11 +1,17 @@
+clear
 echo 'write test'
 time ./write > test.txt
+
 echo ''
 echo 'write1 test'
 time ./write1 > test1.txt
+
 echo ''
 echo 'write2 test'
 time ./write2 > test2.txt
+
 echo ''
 echo 'write3 test'
 time ./write3 > test3.txt
+
+rm test1.txt test2.txt test3.txt
