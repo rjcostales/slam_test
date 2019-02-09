bbb86c13db66703a2eab4186b9d80235684d8a54
Jesse Costales <rjcostales@gmail.com>
2019-02-09 09:32:52 -0700

    define constants

--- math/floatops.c
+++ math/floatops.c
@@ -3,45 +3,48 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define LOOPS 1000000
+#define COUNT 100
+
 int main(void)
 {
-    double doubles[100];
+    double doubles[COUNT];
     double add, sub, multiply, divide;
     clock_t start, end;
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < COUNT; i++)
         doubles[i] = (float) rand() / (float) rand();
-    // for (int i = 0; i < 100; i++)
+    // for (int i = 0; i < COUNT; i++)
     //     printf("%g\n", doubles[i]);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
-            for (int j = 0; j < 100; j++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
                 add = doubles[i] + doubles[j];
     end = clock();
     printf("add %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
-            for (int j = 0; j < 100; j++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
                 sub = doubles[i] - doubles[j];
     end = clock();
     printf("sub %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
-            for (int j = 0; j < 100; j++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
                 multiply = doubles[i] * doubles[j];
     end = clock();
     printf("multiply %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
-            for (int j = 0; j < 100; j++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
                 divide = doubles[i] / doubles[j];
     end = clock();
     printf("divide %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
--- math/mathfunc.c
+++ math/mathfunc.c
@@ -3,55 +3,58 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define LOOPS 1000000
+#define COUNT 100
+
 int main(void)
 {
-    double doubles[100];
+    double doubles[COUNT];
     double sine, cosine, tangent, logrithm, squareroot;
     clock_t start, end;
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < COUNT; i++)
         doubles[i] = (float) rand() / (float) rand();
-    // for (int i = 0; i < 100; i++)
+    // for (int i = 0; i < COUNT; i++)
     //     printf("%g\n", doubles[i]);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             squareroot = sqrt(doubles[i]);
     end = clock();
     printf("sqrt %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             sine = sin(doubles[i]);
     end = clock();
     printf("sin %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             cosine = cos(doubles[i]);
     end = clock();
     printf("cos %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             tangent = tan(doubles[i]);
     end = clock();
     printf("tan %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             logrithm = log(doubles[i]);
     end = clock();
     printf("log %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
 
     start = clock();
-    for (int t = 0; t < 1000000; t++)
-        for (int i = 0; i < 100; i++)
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
             logrithm = log10(doubles[i]);
     end = clock();
     printf("log10 %0.6f\n", (float) (end - start) / (float) CLOCKS_PER_SEC);
