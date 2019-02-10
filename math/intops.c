26ee1b1e4c78c615df24fea43bad2312c641df30
Jesse Costales <rjcostales@gmail.com>
2019-02-09 19:59:02 -0700

    add intops

---
+++ math/intops.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+
+#define LOOPS 10000
+#define COUNT 100
+
+int main(int argc, char *argv[])
+{
+    int integers[COUNT];
+    int result;
+    clock_t start, end;
+
+    setlocale(LC_NUMERIC, "");
+    fprintf(stderr, "%s\t%'d\t%'dx%'d\n", argv[0], LOOPS, COUNT, COUNT);
+
+    for (int i = 0; i < COUNT; i++)
+        integers[i] = rand();
+    // for (int i = 0; i < COUNT; i++)
+    //     printf("%g\n", integers[i]);
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[j];
+    end = clock();
+    fprintf(stderr, "nop %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[i] + integers[j];
+    end = clock();
+    fprintf(stderr, "add %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[i] - integers[j];
+    end = clock();
+    fprintf(stderr, "sub %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[i] * integers[j];
+    end = clock();
+    fprintf(stderr, "mul %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[i] / integers[j];
+    end = clock();
+    fprintf(stderr, "div %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+
+
+    start = clock();
+    for (int t = 0; t < LOOPS; t++)
+        for (int i = 0; i < COUNT; i++)
+            for (int j = 0; j < COUNT; j++)
+                result = integers[i] % integers[j];
+    end = clock();
+    fprintf(stderr, "mod %0.6f\n",
+      (float) (end - start) / (float) CLOCKS_PER_SEC);
+} /* main */
